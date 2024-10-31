1a3296b2a85c2a033bc2bb899437d8c7878018ed
vicmarchesi <victor.marchesi91@gmail.com>
2024-10-31 20:42:11 +0000

    First Commit

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,36 @@
 class Quiz {
     // YOUR CODE HERE:
     //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion(){
+        this.currentQuestionIndex += 1;
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions(){
+        for (let i = this.questions.length - 1; i > 0; i--) { 
+            const j = Math.floor(Math.random() * (i + 1)); 
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]]; 
+    }
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer){
+        if(answer){
+            
+        }
+    }
 
-    // 6. hasEnded()
+    hasEnded(){
+        return this.currentQuestionIndex === this.questions.length;
+    }
 }
